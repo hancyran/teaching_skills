e0ae3ce46a58ccbecf6c9d210020ebe8ad7abbb1
karen9711 <bearbear199711@qq.com>
2018-11-22 11:02:04 +0800

    modified

--- my/php/shoppingHistory.php
+++ my/php/shoppingHistory.php
@@ -3,7 +3,7 @@
 include 'UserInfo.php';
 
 $user = new UserInfo();
-
+$history = $user->getHistory();
  ?>
 
 <!DOCTYPE html>
@@ -39,26 +39,32 @@ $user = new UserInfo();
 
       <div class="col-sm-7">
         <div class="item-list">
-          <div class="item">
-            <div class="row">
-              <div class="col-sm-2">
-                <a class="avator" href="#"></a>
-              </div>
-              <div class="col-sm-10">
-                <div class="title">
-                  数据库
+
+          <?php foreach ($history as $key => $value) { ?>
+
+            <div class="item">
+              <div class="row">
+                <div class="col-sm-2">
+                  <a class="avator" href="#"></a>
                 </div>
-                <div class="course-detail">
-                  <div class="seller">
-                    dalao
+                <div class="col-sm-10">
+                  <div class="title">
+                    <?php echo $value['coursename'] ?>
                   </div>
-                  <div class="status">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                  <div class="course-detail">
+                    <div class="seller">
+                      <?php echo $value['sellername'] ?>
+                    </div>
+                    <div class="status">
+                      <?php echo $value['coursestatus'] ?>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
+            
+          <?php  } ?>
+
 
 
         </div>
